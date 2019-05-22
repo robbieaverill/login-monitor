3644ab6923287549da702703fda45dfc52899d95
Robbie Averill <robbie@silverstripe.com>
2019-05-22 15:55:00 +1200

    Add falsy test for getData()

---
+++ tests/State/GeoResultTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SilverStripe\LoginMonitor\Tests\State;
+
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\LoginMonitor\State\GeoResult;
+
+/**
+ * Note that IPResolvedServiceTest already tests most of the logic in this instance
+ */
+class GeoResultTest extends SapphireTest
+{
+    public function testMissingDataReturnsNull()
+    {
+        $result = new GeoResult([]);
+        $this->assertNull($result->getCountryCode());
+    }
+}
